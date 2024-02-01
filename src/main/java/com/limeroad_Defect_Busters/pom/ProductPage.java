07f398660ddf99a9d0d177f1ae63f5405e330312
PritheepPandurangan <pritheep96.pmv@gmail.com>
2024-02-01 13:46:52 +0530

    Elements added in the productpage

--- src/main/java/com/limeroad_Defect_Busters/pom/ProductPage.java
+++ src/main/java/com/limeroad_Defect_Busters/pom/ProductPage.java
@@ -1,5 +1,13 @@
 package com.limeroad_Defect_Busters.pom;
 
 public interface ProductPage {
+	
+	String click_on_men_xpath ="//p[text()='Men']";
+	String click_on_category_xpath ="//a[@id='men_category']";
+	String click_on_jeans_xpath ="//a[text()='jeans']";
+	String click_on_high_xpath ="//div[text()='High Price']";
+	String click_on_pant_xpath ="//img[@id='19712789']";
+	String click_on_size_xpath ="//span[@id='size_32824454']";
+	String click_on_cart_xpath ="//div[@data-pgn='Add to Cart']";
 
 }
--- src/main/java/com/limeroad_Defect_Busters/pom/ProductPageImpl.java
+++ src/main/java/com/limeroad_Defect_Busters/pom/ProductPageImpl.java
@@ -1,5 +1,70 @@
 package com.limeroad_Defect_Busters.pom;
 
-public class ProductPageImpl {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPageImpl implements ProductPage {
+	
+public WebDriver driver;
+	
+	@FindBy(xpath = click_on_men_xpath)
+	private WebElement element;
+	
+	
+	@FindBy(xpath = click_on_category_xpath)
+    private WebElement men;
+	
+	@FindBy(xpath = click_on_jeans_xpath)
+	private WebElement jeans;
+	
+	@FindBy(xpath = click_on_high_xpath)
+	private WebElement high;
+	
+	@FindBy(xpath = click_on_pant_xpath)
+	private WebElement pant;
+	
+	@FindBy(xpath = click_on_size_xpath)
+	private WebElement size;
+	
+	@FindBy(xpath = click_on_cart_xpath)
+	private WebElement cart;
+	
+
+	public ProductPageImpl(WebDriver driver2) {
+		this.driver = driver2;
+		PageFactory.initElements(driver, this);
+		
+	}
+
+	public WebElement getElement() {
+		return element;
+	}
+
+	public WebElement getMen() {
+		return men;
+	}
+
+	public WebElement getJeans() {
+		return jeans;
+	}
+
+	public WebElement getHigh() {
+		return high;
+	}
+
+	public WebElement getPant() {
+		return pant;
+	}
+
+	public WebElement getSize() {
+		return size;
+	}
+
+	public WebElement getCart() {
+		return cart;
+	}
+
 
 }
